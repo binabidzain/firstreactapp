79c6e0da88f27de6e512f51752acb187c5079da9
Zain Bin Abid <binabidzain@gmail.com>
2021-10-11 16:21:11 +0500

    Commit Message

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,12 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Card(props) {
+  let baseUrl="http://localhost:4000/"
   return (
       <div key={props.cardata.id}  className="card m-2" style={{ width: "18rem"  }}>
         <img
           className="card-img-top"
-          src={`../${props.cardata?.image}`}
-          alt="image cap"
+          src={baseUrl+ props.cardata.image}
+          alt="Card image cap"
         />
         <div className="card-body">
           <h5 className="card-title">{props.cardata?.title}</h5>
@@ -16,7 +17,7 @@ function Card(props) {
           <p>{props.cardata.description}
             
           </p>
-          <Link to={"/product/" + props.cardata.id} className="btn btn-primary">
+          <Link to={"/product/" + props.cardata._id} className="btn btn-primary">
             Buy Now
           </Link>
         </div>
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,12 +1,37 @@
 import Header from "../components/Header";
-import React from "react";
+import React, { useState , useEffect } from "react";
 import { useParams } from "react-router-dom";
 import "../styles/productdetails.css";
 import { getProductById } from "../data/productsdata";
 
 function Product() {
   let params = useParams();
-  let product = getProductById(params.id);
+  let id=params.id;
+  let [product, setProduct]=useState([]);
+  let [err, setErr] = useState(false);
+  let baseUrl="http://localhost:4000/"  
+  
+console.log(product)
+  useEffect(() => {
+
+    fetch(`http://localhost:4000/products/${id}`)
+    
+    .then(res => res.json())
+    .then(data => setProduct(data))
+    
+    .catch(err => {
+      setErr(true);
+      console.log(err.message)
+    })
+    }, []);
+
+  if(err)
+  return <h1> Error in Loading data...</h1>
+
+  if(!product){
+    return <h1> Loading Products.....</h1>
+  }
+  console.log(product);
 
   return (
     <div>
@@ -14,24 +39,24 @@ function Product() {
 
       <div className="main__container d-flex">
         <div className="image__container">
-          <img src={`../${product.image}`} alt="car-img" ></img>
+          <img src={baseUrl+product[0].image} alt="car-img" ></img>
         </div>
 
         <div className="description__container">
-          <h1>{product.title}</h1>
+          <h1>{product[0].title}</h1>
           <div>
           <div>
-            <h3>Price:</h3>
-            {product.price}
+            <h3>Price: {product[0].price} </h3>
+            
           </div>
-            <h3>Model:</h3>
-            {product.model}
-          </div>
-          <div>
-            <h3>Ratings:</h3> &#9734; &#9733;
+            <h3>Model: {product[0].model}</h3>
+            
           </div>
           <div>
-            <h3>Description :</h3> {product.description}
+            <h3>Ratings: {product[0].ratings}</h3> 
+          </div>
+          <div>
+            <h3>Description : {product[0].description}</h3> 
           </div>
           <div className="cart">
             <button type="button" className="btn btn-success">
